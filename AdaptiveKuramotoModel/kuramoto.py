85494b6eb47f3d434ad86904358fcf0ba383c945
Amir Maghoul <maghoula@math.hu-berlin.de>
2022-10-05 17:22:22 +0200

    feature(main): Add class structure to AdaptiveKuramoto

---
+++ AdaptiveKuramotoModel/kuramoto.py
@@ -0,0 +1,32 @@
+""" Module to create adaptive kuramoto model """
+
+import numpy as np
+from scipy.integrate import odeint
+
+class AdaptiveKuramoto():
+
+    def __init__(self):
+        pass
+
+    def dynamics(self):
+        pass
+
+    def solver(self):
+        pass
+
+    def unpack_solver(self):
+        pass
+
+    def plot_matrix(self):
+        pass
+
+    def plot_phase(self):
+        pass
+
+    def order_parameter(self):
+        pass
+
+    def plot_order_parameter(self):
+        pass
+
+
