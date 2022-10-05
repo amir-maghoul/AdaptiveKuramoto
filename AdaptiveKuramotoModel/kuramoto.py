4e1f2b1c91a5ff46377c55222e65a1f8b7a8afd2
Amir Maghoul <maghoula@math.hu-berlin.de>
2022-10-05 17:34:09 +0200

    feature(main): Complete the class structure

--- AdaptiveKuramotoModel/kuramoto.py
+++ AdaptiveKuramotoModel/kuramoto.py
@@ -2,11 +2,44 @@
 
 import numpy as np
 from scipy.integrate import odeint
+import typing
 
 class AdaptiveKuramoto():
 
-    def __init__(self):
-        pass
+    def __init__(self, w, K, ro, N=100, t0=0, t_end=40, epsilon=0.01):
+        """ Adaptive Kuramoto Class
+
+        Paramters
+        ---------
+        w : np.ndarray 1D
+            the natural frequency
+
+        K : np.ndarray 2D
+            the coupling matrix
+
+        ro : float
+            the coupling weight
+
+        N : int
+            number of oscillators
+
+        t0 : float
+            simulation starting time
+
+        t_end : float
+            simulation end time
+
+        epsilon : float
+            slow dynamics coefficient
+        """
+        
+        self.w = w
+        self.K = K
+        self.ro = ro
+        self.N = N
+        self.t0 = t0
+        self.T = t_end
+        self.epsilon = epsilon
 
     def dynamics(self):
         pass
