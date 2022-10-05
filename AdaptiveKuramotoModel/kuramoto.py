91e1efb15c7d4ab9f00f8e231c2f8b3d8d0193fe
Amir Maghoul <maghoula@math.hu-berlin.de>
2022-10-05 18:10:24 +0200

    feature(main): Add dynamics ODE

--- AdaptiveKuramotoModel/kuramoto.py
+++ AdaptiveKuramotoModel/kuramoto.py
@@ -41,11 +41,46 @@ class AdaptiveKuramoto():
         self.T = t_end
         self.epsilon = epsilon
 
-    def dynamics(self):
-        pass
+    def dynamics(self, u, alpha, beta):
+        """ The ODE dynamcs of the Kuramoto model
+            Notice the weight matrix is flattened out and concatenated to the phase vector.
+            Therefore the input is a long vector of both phases and weight matrix
 
-    def solver(self):
-        pass
+
+            Paramters
+            ---------
+            u : np.ndarray 1D
+                u[0:N] are the phases
+                u[N:N**2+N] are the coupling weight matrix flattened out
+
+            alpha : float
+                coupling phase lag
+
+            beta : float
+                coupling adaptation lag
+        """
+
+        # unpacking the inputs
+        phi = u[0:self.N]
+        A = u[self.N:self.N*self.N+self.N]
+        a = alpha*np.ones(self.N)
+        b = alpha*np.ones(self.N)
+
+        # create interaction matrix phi_i - phi_j
+        interaction = phi[:,None] - phi
+
+        phi_dot = self.w + (self.ro*(A*np.sin(interaction + a)).sum(axis=1)/self.N) 
+        A_dot = -self.epsilon(A + np.sin(interaction + b))
+
+        return np.vstack(u, np.reshape(A, (self.N,)))
+
+
+
+
+    def solver(self, u_0, alpha, beta):
+        """ Solves the dynamics ODE"""
+        return
+
 
     def unpack_solver(self):
         pass
@@ -63,3 +98,13 @@ class AdaptiveKuramoto():
         pass
 
 
+def main():
+    u = np.array([1, 2, 3])
+    diff = u[:, None] - u
+    print(diff)
+    print(diff.sum(axis=1))
+
+    
+
+if __name__=="__main__":
+    main()
