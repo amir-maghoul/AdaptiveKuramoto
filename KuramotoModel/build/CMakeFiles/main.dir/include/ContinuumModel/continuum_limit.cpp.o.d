18e06c0ba966d31cefa9815a0ba343d4c99b6018
Amir Maghoul <maghoula@math.hu-berlin.de>
2022-10-31 11:19:19 +0100

    feature(main): Add ignore and delete

--- KuramotoModel/build/CMakeFiles/main.dir/include/ContinuumModel/continuum_limit.cpp.o.d
+++
@@ -1,4 +0,0 @@
-CMakeFiles/main.dir/include/ContinuumModel/continuum_limit.cpp.o: \
- /home/amir/AdaptiveKuramoto/KuramotoModel/include/ContinuumModel/continuum_limit.cpp \
- /usr/include/stdc-predef.h \
- /home/amir/AdaptiveKuramoto/KuramotoModel/include/Headers/continuum_limit.h
