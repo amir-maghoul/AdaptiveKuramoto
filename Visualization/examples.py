9edc8ee0cd961033c04db0c41c11fc61b5258c0c
Amir Maghoul <maghoula@math.hu-berlin.de>
2022-11-29 18:33:39 +0100

    feature(python): First example for continuum limit

--- Visualization/examples.py
+++ Visualization/examples.py
@@ -1,4 +1,5 @@
 from visualize import *
+import sys
 
 def example_autonamted_all(n, t):
     file_loc = "/home/amir/AdaptiveKuramoto/KuramotoModel/txt_outputs/clustering_file.txt"
@@ -9,22 +10,52 @@ def example_autonamted_all(n, t):
     plot_order_parameter(order_parameter)
     plt.show()
 
-def example_automated_phase_and_order_parameter():
+def example_automated_order_parameter():
     file_loc = "/home/amir/AdaptiveKuramoto/KuramotoModel/txt_outputs/order_parameter_file.txt"
     order_parameter = read_vector_to_array(file_loc)
     plot_order_parameter(order_parameter)
     plt.show()
 
 def example_plotmatrix_large_data_positive_ab():
-    file_loc = "/home/amir/AdaptiveKuramoto/KuramotoModel/txt_outputs/alphaPbetaP.txt"
+    file_loc = "/home/amir/AdaptiveKuramoto/KuramotoModel/txt_outputs/alphaPbetaP.txt" # 10000 end time with 100000 timesteps
     n = 100
     adj_matrix = read_matrix_to_array(file_loc, n)
     plot_matrix(adj_matrix, 100)
     plt.show()
 
+def example_plotmatrix_not_so_large_data_positive_ab():
+    file_loc = "/home/amir/AdaptiveKuramoto/KuramotoModel/txt_outputs/a0_3p_b0_53p.txt" # 1000 end time with 10000 timesteps
+    adj_matrix = read_matrix_to_array(file_loc, 100)
+    plot_matrix(adj_matrix, 1)
+    plt.show()
+
+def example_plot_phases():
+    file_loc = "/home/amir/AdaptiveKuramoto/KuramotoModel/txt_outputs/phases_file.txt"
+    phases = read_vector_to_array(file_loc)
+    plt.plot(np.diff(phases[:, 1])/0.01)
+    plt.show()
+
+
+def example_contlim_weights(n, t):
+    file_loc = "/home/amir/AdaptiveKuramoto/KuramotoModel/txt_outputs/contlim_clustering_file.txt"
+    adj_matrix = read_matrix_to_array(file_loc, n)
+    plot_matrix(adj_matrix, t)
+    plt.show()
+
+def Ring(x, y, h):
+    if min(abs(x-y), 1-abs(x-y)) <= h:
+        return 1
+    else:
+        return 0
+
+
+
 if __name__ == "__main__":
     timestep = 100
-    n = 100
-    # automated_order_parameter()
-    # example_autonamted_all(n, timestep)
-    example_automated_phase_and_order_parameter(0)
+    n = 50
+    # example_plot_phases()
+    # example_plotmatrix_not_so_large_data_positive_ab()
+    # example_autonamted_all(100, 50)
+    # example_automated_order_parameter()
+    # example_plotmatrix_large_data_positive_ab()
+    example_contlim_weights(50, 0)
