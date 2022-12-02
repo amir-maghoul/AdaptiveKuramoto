ba1848a58512cdf7d7f8c0af5f4851a048c3b529
Amir Maghoul <maghoula@math.hu-berlin.de>
2022-12-02 02:38:44 +0100

    feature(main): Add OpenMP to cmake

--- KuramotoModel/main.cpp
+++ KuramotoModel/main.cpp
@@ -4,45 +4,70 @@
 #include "include/Headers/solvers.h"
 #include "visualization/visualization.h"
 #include "include/Headers/integrators.h"
-#include "examples/examples.h"
+#include "include/Headers/integral_solver.h"
+#include "examples/headers/ds_examples.h"
+#include "examples/headers/cl_examples.h"
 #include <eigen/Dense>
 #include <vector>
 #include <typeinfo>
 #include <iostream>
+#include <functional>
+#include <chrono>
 
 using namespace std;
 using namespace Eigen;
 
-double function(double x, double y)
-{
-    return 1;
-};
 
-auto f = [] (double x, double y)
-{
-    return x*x + y*y;
-};
-auto g = [] (double x)
-{
-    return 1;
-};
 
 int main(){
+    omp_set_num_threads(8);
+    Eigen::setNbThreads(8);
 
+    // VeryLargeRingGraphLongSimulationShort();
+    QuiteLargeRingGraphLongSimulationShort();
+    // LargeRingGraphLongSimulationShort();
+    // RandomGraphSimulation();
+    // SmallRingGraphLongSimulation2();
+    // SmallRingGraphLongSimulation();
+    // SmallRingTopology();
+    // SaveMatrix();
+
+    // double result = IntegralSolvers::Trap1D(g, 0, 1, 100, 1);
+    // cout << result << endl;
+
+    // Integrator integral;
+    // integral.n = 1;
+
+    // double result;
+    // std::vector<double> discrete = integral.DiscretizeSpace();
     
-    Integrator integral;
-    integral.n = 1;
+    // result = integral.TrapozoidalRule1D(g);
+    // cout << result << endl;
 
-    double result;
-    std::vector<double> discrete = integral.DiscretizeSpace();
-    
-    result = integral.TrapozoidalRule1D(g);
-    cout << result << endl;
+    // cout << "Now 2D........" << endl;
+    // Eigen::VectorXd V;
+    // V = integral.TrapozoidalRule1Din2D(f, "x");
+    // cout << V << endl;
 
-    cout << "Now 2D........" << endl;
-    Eigen::VectorXd V;
-    V = integral.TrapozoidalRule1Din2D(f, "x");
-    cout << V << endl;
+    // Eigen::VectorXd W(5);
+    // W << 1, 2, 3, 4, 5;
+    // cout << W << endl;
+    // cout << "----------------" << endl;
+
+    // Eigen::VectorXd U(4);
+    // U = W.segment(1, W.size()-1) + W.segment(0, W.size()-1);
+
+    // cout << U << endl;
+    // const int n = 5;
+    // typedef Eigen::Matrix<double, n, n> Matrix5f;
+    // Eigen::MatrixXd W;
+    // W = Matrix5f::Random();
+
+    // Eigen::VectorXd res = IntegralSolvers::TrapMatrixin1D(W, 0, 1, 50, 2, "x");
+
+    // cout << W << endl;
+    // cout << res << endl;
+
 
     // LotkaVolterra();
     // TestClustering();
@@ -50,6 +75,5 @@ int main(){
     // TestOrderParameter();
 
 
-
     return 0;
-}
+};
