291d68f7d9198bcb9a3843ae909382a28c59dbb1
Amir Maghoul <maghoula@math.hu-berlin.de>
2023-03-13 18:49:56 +0100

    feature(analysis): Add first draft error calculator

--- KuramotoModel/include/Analysis/analysis.cpp
+++ KuramotoModel/include/Analysis/analysis.cpp
@@ -1,5 +1,16 @@
 #include "include/Headers/analysis.h"
 
+/**
+ * Convert a std::vector of matrices into a std::vector of matrices which are discretized continuous
+ * extension of the original matrices.
+ * 
+ * @param VectorData        std::vector<Eigen::MatrixXd>     The input vector of matrices to be converted
+ * @param ReferenceN        int     The number of discretization point after continuous extension
+ * @param isMatrix          bool    Boolean flag to check if the vector contains 1D or 2D matrices
+ * 
+ * @return  std::vector<Eigen::MatrixXd> The new vector of discretized continuously extended matrices
+ * 
+*/
 std::vector<Eigen::MatrixXd> ContinuousExtensionConverter(std::vector<Eigen::MatrixXd> VectorData, int ReferenceN, bool isMatrix){
 
     auto ContExt1D = [ReferenceN](Eigen::MatrixXd Matrix){
@@ -10,7 +21,7 @@ std::vector<Eigen::MatrixXd> ContinuousExtensionConverter(std::vector<Eigen::Mat
         return DiscretizedContinuousExtension2D(Matrix, ReferenceN);
     };
 
-    std::vector<Eigen::MatrixXd> result;
+    std::vector<Eigen::MatrixXd> result(VectorData.size());
 
     if (isMatrix){
         std::transform(VectorData.begin(), VectorData.end(), result.begin(), ContExt2D);
@@ -19,4 +30,36 @@ std::vector<Eigen::MatrixXd> ContinuousExtensionConverter(std::vector<Eigen::Mat
 
     }
     return result;
+}
+
+std::vector<double> WeightL1Error(std::vector<Eigen::MatrixXd> DiscreteMatrixVector, std::vector<Eigen::MatrixXd> ContlimMatrixVector){
+    int size = DiscreteMatrixVector.at(0).rows();
+    std::vector<double> Error(DiscreteMatrixVector.size());
+
+    auto TrapozoidSolver = [size](Eigen::MatrixXd M){
+        return IntegralSolvers::TrapMatrixin1D(M, 0, 1, size, 1, "x");
+    };
+
+    for (size_t i=0; i<Error.size();++i){
+            Error[i] = TrapozoidSolver((DiscreteMatrixVector.at(i) - ContlimMatrixVector.at(i)).cwiseAbs()).maxCoeff();    
+    };
+    return Error;
+}
+
+void ReadConvertWrite(std::string ReadFileLoc, std::string WriteFileLoc, std::string GraphType, int n, int ReferenceN, bool isMatrix, double tend){
+    std::string ContLimSignatureName = "contlim_" + GraphType + "_with_" + std::to_string(ReferenceN) + "_oscillators_";
+    std::string DiscreteSignatureName = "discrete_" + GraphType + "_with_" + std::to_string(n) + "_oscillators_";
+
+    std::string ContlimWeightsFileName = ReadFileLoc + "tend_" + std::to_string((int) tend) + "/" + ContLimSignatureName + "tend_" +std::to_string((int) tend) + ".txt";
+    std::string DiscreteWeightsFileName = ReadFileLoc + "tend_" + std::to_string((int) tend) + "/" + DiscreteSignatureName + "tend_" +std::to_string((int) tend) + ".txt";
+
+    std::string ContlimPhasesFileName = ReadFileLoc + "tend_" + std::to_string((int) tend) + "/" + ContLimSignatureName + "phases_tend_" +std::to_string((int) tend) + ".txt";
+    std::string DiscretePhasesFileName = ReadFileLoc + "tend_" + std::to_string((int) tend) + "/" + DiscreteSignatureName + "phases_tend_" +std::to_string((int) tend) + ".txt";
+
+    std::cout << ContlimWeightsFileName << std::endl;
+    std::cout << DiscreteWeightsFileName << std::endl;
+    std::cout << ContlimPhasesFileName << std::endl;
+    std::cout << DiscretePhasesFileName << std::endl;
+
+
 }
--- KuramotoModel/include/Headers/analysis.h
+++ KuramotoModel/include/Headers/analysis.h
@@ -4,8 +4,10 @@
 #include "iofunctions.h"
 #include <algorithm>
 #include "continuous_extension.h"
+#include "integral_solver.h"
 
 std::vector<Eigen::MatrixXd> ContinuousExtensionConverter(std::vector<Eigen::MatrixXd> VectorData,  int ReferenceN, bool isMatrix);
-
+void ReadConvertWrite(std::string ReadFileLoc, std::string WriteFileLoc, std::string GraphType, int n, int ReferenceN, bool isMatrix, double tend);
+std::vector<double> WeightL1Error(std::vector<Eigen::MatrixXd> DiscreteMatrix, std::vector<Eigen::MatrixXd> ContlimMatrix);
 
 #endif
